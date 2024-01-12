5eaaa401f679e2f87784043e2be4790bf3a1a299
sonoMinni <minniellid40@gmail.com>
2024-01-12 11:51:30 +0100

    manca poco

--- ceapro-project/ceapro_site/src/app/app.component.ts
+++ ceapro-project/ceapro_site/src/app/app.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { NMisionComponent } from './nmision/nmision.component';
+import { VisionComponent } from './vision/vision.component';
+import { ValoresComponent } from './valores/valores.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, NMisionComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, NMisionComponent, VisionComponent, ValoresComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
